e0f16f3eb432735905f3fc8c34be1004afec1c21
Tommy <tommy-sc@live.no>
2022-05-15 17:23:34 +0200

    Update 15 Mai
    Lære om å bruke Objects, Counstructors og Methods i en mer organisert kode

---
+++ Bossfight/GameCharacter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight
+{
+    internal class GameCharacter
+    {
+        // Properties for hero and boss
+        public int Health;
+        public int Strength;
+        public int Stamina;
+        //private int _MaxStamina;
+
+        // Constructor 
+        public GameCharacter(int aHealth, int aStrength, int aStamina) // <-- This is the constructor with parameters. "a" for Arguments
+        {
+            Health = aHealth;
+            Strength = aStrength;
+            Stamina = aStamina;
+        }
+
+        // Methods
+        public void Fight(int aStrength)
+        {
+            if (Stamina == 0)
+            {
+                Console.WriteLine("Out of stamina!!");
+                
+            }
+        }
+
+    }
+}
---
+++ Bossfight/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random bStrength = new Random();
+
+        var Hero = new GameCharacter(100, 20, 40); // <-- The stats of the hero: Health = 100, Strength = 20 and Stamina = 40
+        var Boss = new GameCharacter(400, 0 - 30, 10); // <-- The stats of the boss: Health = 400, Strength = (random0, 30) and Stamina = 10
+        
+
+        Console.WriteLine("Hero health: " + Hero.Health);
+        Console.WriteLine("Boss health: " + Boss.Health);
+        Console.ReadLine();
+    }
+
+}
